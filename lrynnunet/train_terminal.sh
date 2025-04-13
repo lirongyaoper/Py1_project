9443da3f23226856d26ce472a834249ebf697bf7
lirongyaoper <lirongyaoper@163.com>
2025-04-13 13:44:07 +0800

    update data at 20250413134407

--- lrynnunet/train_terminal.sh
+++ lrynnunet/train_terminal.sh
@@ -27,31 +27,39 @@ group_one() {
 
 group_two() {
   echo "2D U-Net 训练"
-  CUDA_VISIBLE_DEVICES=0 nnUNetv2_train 001 2d 0 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=1 nnUNetv2_train 001 2d 1 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=2 nnUNetv2_train 001 2d 2 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=3 nnUNetv2_train 001 2d 3 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=4 nnUNetv2_train 001 2d 4 -p nnUNetResEncUNetLPlans --npz
+#  CUDA_VISIBLE_DEVICES=0 nnUNetv2_train -d 001 2d 0 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=1 nnUNetv2_train -d 001 2d 1 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=2 nnUNetv2_train -d 001 2d 2 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=3 nnUNetv2_train -d 001 2d 3 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=4 nnUNetv2_train -d 001 2d 4 -p nnUNetResEncUNetLPlans --npz
+
+  CUDA_VISIBLE_DEVICES=0 nnUNetv2_train -tr lryTrainer -d 001 2d 0 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=1 nnUNetv2_train -tr lryTrainer -d 001 2d 1 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=2 nnUNetv2_train -tr lryTrainer -d 001 2d 2 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=3 nnUNetv2_train -tr lryTrainer -d 001 2d 3 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=4 nnUNetv2_train -tr lryTrainer -d 001 2d 4 -p nnUNetResEncUNetLPlans --npz
   echo "2D U-Net 训练完成！！！"
 }
 
 group_three(){
   echo "3d_lowres训练"
-  CUDA_VISIBLE_DEVICES=0 nnUNetv2_train 001 3d_lowres 0 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=1 nnUNetv2_train 001 3d_lowres 1 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=2 nnUNetv2_train 001 3d_lowres 2 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=3 nnUNetv2_train 001 3d_lowres 3 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=4 nnUNetv2_train 001 3d_lowres 4 -p nnUNetResEncUNetLPlans --npz
+#  CUDA_VISIBLE_DEVICES=0 nnUNetv2_train -d 001 3d_lowres 0 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=1 nnUNetv2_train -d 001 3d_lowres 1 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=2 nnUNetv2_train -d 001 3d_lowres 2 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=3 nnUNetv2_train -d 001 3d_lowres 3 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=4 nnUNetv2_train -d 001 3d_lowres 4 -p nnUNetResEncUNetLPlans --npz
+
+  CUDA_VISIBLE_DEVICES=0 nnUNetv2_train -tr lryTrainer -d 001 3d_lowres 0 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=1 nnUNetv2_train -tr lryTrainer -d 001 3d_lowres 1 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=2 nnUNetv2_train -tr lryTrainer -d 001 3d_lowres 2 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=3 nnUNetv2_train -tr lryTrainer -d 001 3d_lowres 3 -p nnUNetResEncUNetLPlans --npz & CUDA_VISIBLE_DEVICES=4 nnUNetv2_train -tr lryTrainer -d 001 3d_lowres 4 -p nnUNetResEncUNetLPlans --npz
   echo "3d_lowres训练完成！！！"
 }
 
 group_four(){
   echo "3d_cascade_fullres训练"
-  CUDA_VISIBLE_DEVICES=0 nnUNetv2_train 001 3d_cascade_fullres 0 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=1 nnUNetv2_train 001 3d_cascade_fullres 1 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=2 nnUNetv2_train 001 3d_cascade_fullres 2 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=3 nnUNetv2_train 001 3d_cascade_fullres 3 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=4 nnUNetv2_train 001 3d_cascade_fullres 4 -p nnUNetResEncUNetLP1ans --npz
+#  CUDA_VISIBLE_DEVICES=0 nnUNetv2_train -d 001 3d_cascade_fullres 0 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=1 nnUNetv2_train -d 001 3d_cascade_fullres 1 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=2 nnUNetv2_train -d 001 3d_cascade_fullres 2 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=3 nnUNetv2_train -d 001 3d_cascade_fullres 3 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=4 nnUNetv2_train -d 001 3d_cascade_fullres 4 -p nnUNetResEncUNetLP1ans --npz
+
+  CUDA_VISIBLE_DEVICES=0 nnUNetv2_train -tr lryTrainer -d 001 3d_cascade_fullres 0 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=1 nnUNetv2_train -tr lryTrainer -d 001 3d_cascade_fullres 1 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=2 nnUNetv2_train -tr lryTrainer -d 001 3d_cascade_fullres 2 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=3 nnUNetv2_train -tr lryTrainer -d 001 3d_cascade_fullres 3 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=4 nnUNetv2_train -tr lryTrainer -d 001 3d_cascade_fullres 4 -p nnUNetResEncUNetLP1ans --npz
   echo "3d_cascade_fullres训练完成！！！"
 }
 
 group_five(){
   echo "3d_fullres 训练"
-  CUDA_VISIBLE_DEVICES=0 nnUNetv2_train 001 3d_fullres 0 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=1 nnUNetv2_train 001 3d_fullres 1 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=2 nnUNetv2_train 001 3d_fullres 2 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=3 nnUNetv2_train 001 3d_fullres 3 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=4 nnUNetv2_train 001 3d_fullres 4 -p nnUNetResEncUNetLP1ans --npz
+#  CUDA_VISIBLE_DEVICES=0 nnUNetv2_train -d 001 3d_fullres 0 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=1 nnUNetv2_train -d 001 3d_fullres 1 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=2 nnUNetv2_train -d 001 3d_fullres 2 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=3 nnUNetv2_train -d 001 3d_fullres 3 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=4 nnUNetv2_train -d 001 3d_fullres 4 -p nnUNetResEncUNetLP1ans --npz
+
+  CUDA_VISIBLE_DEVICES=0 nnUNetv2_train -tr lryTrainer -d 001 3d_fullres 0 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=1 nnUNetv2_train -tr lryTrainer -d 001 3d_fullres 1 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=2 nnUNetv2_train -tr lryTrainer -d 001 3d_fullres 2 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=3 nnUNetv2_train -tr lryTrainer -d 001 3d_fullres 3 -p nnUNetResEncUNetLP1ans --npz & CUDA_VISIBLE_DEVICES=4 nnUNetv2_train -tr lryTrainer -d 001 3d_fullres 4 -p nnUNetResEncUNetLP1ans --npz
   echo "3d_fullres 训练完成！！！"
 }
 
 
 START_TIME=$(date +%s)
-group_one || exit 1
+#group_one || exit 1
 group_two || exit 1
 group_three || exit 1
 group_four || exit 1
