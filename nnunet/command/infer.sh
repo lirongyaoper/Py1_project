2862dc4f7510809c358c8c87f2f0b60147085484
lirongyaoper <lirongyaoper@163.com>
2025-06-11 22:01:22 +0800

    update data at 20250611220122

--- nnunet/command/infer.sh
+++ nnunet/command/infer.sh
@@ -19,3 +19,17 @@ nnUNetv2_predict -d Dataset001_Lung -i /home/lirongyao0916/Documents/infer_nnune
 ***Once inference is completed, run postprocessing like this:***
 
 nnUNetv2_apply_postprocessing -i /mnt/result/input/  -o  /mnt/result/output/ -pp_pkl_file /data/nnunet/nnUNet_results/Dataset001_Lung/nnUNetTrainer__nnUNetResEncUNetLPlans__3d_fullres/crossval_results_folds_0_1_2_3_4/postprocessing.pkl -np 8 -plans_json /data/nnunet/nnUNet_results/Dataset001_Lung/nnUNetTrainer__nnUNetResEncUNetLPlans__3d_fullres/crossval_results_folds_0_1_2_3_4/plans.json
+
+
+
+
+
+//推理：
+//Dataset002_Lung 非级联
+nnUNetv2_predict -i /home/lirongyao0916/Documents/infer_nnunet/input/ -o /home/lirongyao0916/Documents/infer_nnunet/output/  -d Dataset002_Lung -c 3d_fullres
+
+//Dataset001_Lung 非级联
+nnUNetv2_predict -d Dataset001_Lung -i /home/lirongyao0916/Documents/infer_nnunet/input/ -o /home/lirongyao0916/Documents/infer_nnunet/output/ -f  0 1 2 3 4 -tr nnUNetTrainer -c 3d_fullres -p nnUNetResEncUNetLPlans
+
+//Dataset001_Lung 级联
+nnUNetv2_predict -d Dataset001_Lung -i /home/lirongyao0916/Documents/infer_nnunet/input/ -o /home/lirongyao0916/Documents/infer_nnunet/output/ -f  0 1 2 3 4 -tr nnUNetTrainer -c 3d_lowres -p nnUNetResEncUNetLPlans && nnUNetv2_predict -d Dataset001_Lung -i /home/lirongyao0916/Documents/infer_nnunet/input/ -o /home/lirongyao0916/Documents/infer_nnunet/output/ -c 3d_cascade_fullres -tr nnUNetTrainer -p nnUNetResEncUNetLPlans -prev_stage_predictions /home/lirongyao0916/Documents/infer_nnunet/medput/
